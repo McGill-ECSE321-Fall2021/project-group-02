e9770c417dbbdef8e1a80efc4a175cff045b12ce
ju-liechen <julie.chen@mail.mcgill.ca>
2021-10-14 17:13:55 -0400

    updated java and removed user role query

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/UserRepository.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/UserRepository.java
@@ -9,11 +9,9 @@ public interface UserRepository extends CrudRepository<User, Integer>{
 	List<User> findUserByFirstAndLastName(String firstName, String lastName);
 	List<User> findUserByFirstName(String firstName);
 	List<User> findUserByLastName(String lastName);
-	List<User> findUserByRole(String role);
 
 	boolean existsUserByID(Integer iD);
 	boolean existsUserByFirstAndLastName(String firstName, String lastName);
 	boolean existsUserByFirstName(String firstName);
 	boolean existsUserByLastName(String lastName);
-	boolean existsUserByRole(String role);
 } 
