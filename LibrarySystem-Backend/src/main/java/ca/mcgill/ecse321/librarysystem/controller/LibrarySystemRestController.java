08758d6b204cafbafcc94dd966f5b8d83e8b159d
sami <77645479+samiad2788@users.noreply.github.com>
2021-10-28 20:30:03 -0300

    Added throwing of errors in Borrow Items

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/LibrarySystemRestController.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/LibrarySystemRestController.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-import ca.mcgill.ecse321.librarysystem.service.LibrarySystemService;
+import ca.mcgill.ecse321.librarysystem.service.BorrowItemsService;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class LibrarySystemRestController {
 	@Autowired
-	private LibrarySystemService service;
+	private BorrowItemsService service;
 }
