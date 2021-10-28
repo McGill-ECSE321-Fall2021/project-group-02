770aaad8cd384f5ff13da085163ecb2afcae4b50
Hy-Cho <hyunbum.cho@mail.mcgill.ca>
2021-10-28 01:55:28 -0400

    added service class for online acc

---
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse/librarysystem/service/CreateOnlineAccountService.java
@@ -0,0 +1,138 @@
+package ca.mcgill.ecse.librarysystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.librarysystem.dao.OnlineAccountRepository;
+import ca.mcgill.ecse321.librarysystem.dao.PatronRepository;
+import ca.mcgill.ecse321.librarysystem.dao.UserEntityRepository;
+import ca.mcgill.ecse321.librarysystem.model.OnlineAccount;
+import ca.mcgill.ecse321.librarysystem.model.Patron;
+import ca.mcgill.ecse321.librarysystem.model.Person;
+import ca.mcgill.ecse321.librarysystem.model.UserEntity;
+
+@Service
+public class CreateOnlineAccountService {
+	// --------------------------------------------------------------
+	// note: removed email verification as currently it is impossible to send emails
+	// need to add balance to patron class
+	@Autowired
+	PatronRepository patronRepository;
+	@Autowired
+	OnlineAccountRepository onlineAccountRepository;
+	@Autowired
+	UserEntityRepository userEntityRepository;
+	@Transactional
+	public OnlineAccount createOnlineAccountNewUser(Person person, String address, String city, String username, String password, String email) throws IllegalArgumentException {
+		String error = "";
+		
+		// create online account
+		// verify username, email exists
+		if(!verifyUsernameExists(username)) error += "Username is already used.\n";
+		if(!verifyEmailExists(email)) error += "Email is already used.\n";
+		// verify email address
+		if(!verifyEmail(email)) error += "Invalid email address.\n";
+		// check password
+		if(!checkPassword(password)) error += "Password too short or too long.\n";
+		if (error.length() > 0) throw new IllegalArgumentException(error);
+		
+		// create patron and online account
+		Patron patron = new Patron();
+		patron.setAddress(address);
+		patron.setCity(city);
+		patron.setPerson(person);
+		boolean withinCity = (city == "Montreal");
+		if(!withinCity) patron.setBalance(patron.getBalance() + 50);
+		OnlineAccount account = new OnlineAccount();
+		account.setEmail(email);
+		account.setPassword(password);
+		account.setUser(patron);
+		patron.setOnlineAccount(account);
+		onlineAccountRepository.save(account);
+		patronRepository.save(patron);
+		// -----------------------------------------
+		// do I need to save patron to entity repo as well?
+		userEntityRepository.save(patron);
+		
+		return account;
+	}
+	public OnlineAccount createOnlineAccountExistingUser(int id, String username, String password, String email) throws IllegalArgumentException {
+		String error = "";
+		// verify ID
+		UserEntity user = userEntityRepository.findUserEntityById(id);
+		if (user==null) {
+			error += "ID does not exist.\n";
+			throw new IllegalArgumentException(error);
+		}
+		
+		// user already has an online account
+		if(user.getOnlineAccount()!=null) {
+			error += "ID has an associated online account already.\n";
+			throw new IllegalArgumentException(error);
+		}
+		
+		// create online account
+		// verify username, email exists
+		if(!verifyUsernameExists(username)) error += "Username is already used.\n";
+		if(!verifyEmailExists(email)) error += "Email is already used.\n";
+		// verify email address
+		if(!verifyEmail(email)) error += "Invalid email address.\n";
+		// check password
+		if(!checkPassword(password)) error += "Password too short or too long.\n";
+		if (error.length() > 0) throw new IllegalArgumentException(error);
+		
+		OnlineAccount account = new OnlineAccount();
+		account.setEmail(email);
+		account.setPassword(password);
+		account.setUser(user);
+		onlineAccountRepository.save(account);
+		user.setOnlineAccount(account);
+		// --------------------------------------------------------------
+		// IMPORTANT: not sure if this creates another user or if it updates original user
+		userEntityRepository.save(user);
+		return account;
+	}
+	
+	private boolean verifyEmail(String email) {
+		boolean valid = true;
+	    if(!email.contains("@")) {
+	      valid = false; 
+	    } else if (!email.contains(".")) {
+	      valid = false;
+	    } else if (email.charAt(0)=='@') { // starts with @
+	      valid = false;
+	    } else if (email.charAt(email.length()-1) == '.') { // ends with .
+	      valid = false;
+	    } else if (email.charAt(email.length() - 1) == '@' || email.charAt(email.length() - 2) == '@') { // @ is last or second last character
+	      valid = false;
+	    } else if(!email.substring(email.indexOf('@') + 2, email.length() - 1).contains(".")) { 
+	      // . has to be at least two characters after @ (need at least one character between them)
+	      valid = false;
+	    }
+	    return valid;
+	}
+	
+	private boolean verifyUsernameExists(String username) {
+		boolean valid = true;
+		OnlineAccount account = onlineAccountRepository.findOnlineAccountByUsername(username);
+		// if username is already used
+		if (account != null) valid = false;
+		return valid;
+	}
+	
+	private boolean verifyEmailExists(String email) {
+		boolean valid = true;
+		OnlineAccount account = onlineAccountRepository.findOnlineAccountByEmail(email);
+		// if email is already used
+		if (account != null) valid = false;
+		return valid;
+	}
+	
+	private boolean checkPassword(String password){
+		boolean valid = true;
+		int length = password.length();
+		if (length<8 || length >16) valid = false;
+		return valid;
+	}
+}
