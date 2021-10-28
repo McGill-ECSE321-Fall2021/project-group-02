40934aae5e4e2d3422086cdd24c62a4cc8696cdd
Hy-Cho <hyunbum.cho@mail.mcgill.ca>
2021-10-28 01:55:28 -0400

    merge conflict resolved

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dto/OnlineAccountDto.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dto/OnlineAccountDto.java
@@ -1,5 +1,25 @@
 package ca.mcgill.ecse321.librarysystem.dto;
 
 public class OnlineAccountDto {
-
+	private String username;
+	private String password;
+	private String email;
+	
+	public OnlineAccountDto() {
+		
+	}
+	
+	public OnlineAccountDto(String username, String password, String email) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
 }
--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dto/PatronDto.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dto/PatronDto.java
@@ -1,5 +1,29 @@
 package ca.mcgill.ecse321.librarysystem.dto;
 
 public class PatronDto {
-
+	private int id;
+	private String address;
+	private String city;
+	
+	public PatronDto() {
+		
+	}
+	
+	public PatronDto(int id, String address, String city) {
+		this.id = id;
+		this.address = address;
+		this.city = city;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public String getCity() {
+		return city;
+	}
 }
