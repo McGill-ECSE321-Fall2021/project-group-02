c10964d87919d2c426a3a9fb3d78d30fac72a685
sami <77645479+samiad2788@users.noreply.github.com>
2021-10-31 12:35:13 -0300

    Combined John's ArchiveItems part with Julie's ReturnedItems

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/LibrarySystemRestController.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/LibrarySystemRestController.java
@@ -4,11 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-import ca.mcgill.ecse321.librarysystem.service.BorrowItemsService;
 
 @CrossOrigin(origins = "*")
 @RestController
 public class LibrarySystemRestController {
-	@Autowired
-	private BorrowItemsService service;
 }
