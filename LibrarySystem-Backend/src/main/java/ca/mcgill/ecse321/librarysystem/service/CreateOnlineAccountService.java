4ac45ccf02949ef28c7d315e80936c5ae54e8818
Hy-Cho <hyunbum.cho@mail.mcgill.ca>
2021-10-31 03:45:47 -0400

    updated service complying to new model

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/CreateOnlineAccountService.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/CreateOnlineAccountService.java
@@ -9,7 +9,6 @@ import ca.mcgill.ecse321.librarysystem.dao.PatronRepository;
 import ca.mcgill.ecse321.librarysystem.dao.UserEntityRepository;
 import ca.mcgill.ecse321.librarysystem.model.OnlineAccount;
 import ca.mcgill.ecse321.librarysystem.model.Patron;
-import ca.mcgill.ecse321.librarysystem.model.Person;
 import ca.mcgill.ecse321.librarysystem.model.UserEntity;
 
 @Service
@@ -23,7 +22,7 @@ public class CreateOnlineAccountService {
 	@Autowired
 	UserEntityRepository userEntityRepository;
 	@Transactional
-	public OnlineAccount createOnlineAccountNewUser(Person person, String address, String city, String username, String password, String email) throws IllegalArgumentException {
+	public OnlineAccount createOnlineAccountNewUser(String firstName, String lastName, String address, String city, String username, String password, String email) throws IllegalArgumentException {
 		String error = "";
 		
 		// create online account
@@ -40,12 +39,18 @@ public class CreateOnlineAccountService {
 		Patron patron = new Patron();
 		patron.setAddress(address);
 		patron.setCity(city);
-		patron.setPerson(person);
+		patron.setFirstName(firstName);
+		patron.setLastName(lastName);
 		boolean withinCity = (city == "Montreal");
-		if(!withinCity) patron.setBalance(patron.getBalance() + 50);
+		if(!withinCity) {
+			patron.setBalance(patron.getBalance() + 50);
+		} else {
+			patron.setBalance(0);
+		}
 		OnlineAccount account = new OnlineAccount();
 		account.setEmail(email);
 		account.setPassword(password);
+		account.setUsername(username);
 		account.setUser(patron);
 		patron.setOnlineAccount(account);
 		onlineAccountRepository.save(account);
