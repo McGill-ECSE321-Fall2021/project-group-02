79c6dcbc95877a0769f73a735815d4c92b5d5370
sami <77645479+samiad2788@users.noreply.github.com>
2021-11-05 18:44:29 -0300

    added tests

--- LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/service/TestBorrowItemsService.java
+++ LibrarySystem-Backend/src/test/java/ca/mcgill/ecse321/librarysystem/service/TestBorrowItemsService.java
@@ -1,8 +1,12 @@
 package ca.mcgill.ecse321.librarysystem.service;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -11,6 +15,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import ca.mcgill.ecse321.librarysystem.dao.ItemRepository;
 import ca.mcgill.ecse321.librarysystem.dao.PatronRepository;
+import ca.mcgill.ecse321.librarysystem.model.Item;
+import ca.mcgill.ecse321.librarysystem.model.Patron;
 
 
 
@@ -27,19 +33,92 @@ public class TestBorrowItemsService {
 	private ItemService service;
 	
 	private static final int EXISTINGPERSON_ID = 0;
-	private static final int NONEXISTINGPERSON_ID = -1;
+	private static final int NONEXISTINGPERSON_ID = 455;
+	
+	private static final String testString="tester";
+	private static final String wrongString="wrong";
+	
 
-	/*
+	
 	@BeforeEach
 	public void setMockOutput() {
 		lenient().when(patronDao.findPatronById(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
+			//may have to add online account
 			if(invocation.getArgument(0).equals(EXISTINGPERSON_ID)) {
-				
+				Patron patron= new Patron();
+				patron.setAddress(testString);
+				patron.setBalance(EXISTINGPERSON_ID);
+				patron.setCity(testString);
+				patron.setFirstName(testString);
+				patron.setLastName(testString);
+				patron.setId(EXISTINGPERSON_ID);
+				return patron;
+			}
+			else {
+				return null;
 			}
 			
 		});
+		
+		lenient().when(patronDao.findPatronByAddress(anyString())).thenAnswer((InvocationOnMock invocation) -> {
+			if(invocation.getArgument(0).equals(testString)) {
+				Patron patron= new Patron();
+				patron.setAddress(testString);
+				patron.setBalance(EXISTINGPERSON_ID);
+				patron.setCity(testString);
+				patron.setFirstName(testString);
+				patron.setLastName(testString);
+				patron.setId(EXISTINGPERSON_ID);
+			
+				return patron;
+			}
+			else {
+				return null;
+			}
+			
+		});
+		
+		lenient().when(patronDao.findById(anyInt())).thenAnswer((InvocationOnMock invocation) -> {
+			if(invocation.getArgument(0).equals(EXISTINGPERSON_ID)) {
+				Patron patron= new Patron();
+				patron.setAddress(testString);
+				patron.setBalance(EXISTINGPERSON_ID);
+				patron.setCity(testString);
+				patron.setFirstName(testString);
+				patron.setLastName(testString);
+				patron.setId(EXISTINGPERSON_ID);
+			
+				return patron;
+			}
+			else {
+				return null;
+			}
+			
+		});
+		
+		
+		
 	}
-	*/
+	
+	@Test
+	public void testBorrowItemNegativeId() {
+		int patronId=0;
+		String itemName="The Mockingbird";
+		int itemId=-2;
+		Item item=null;
+		
+		String error=null;
+		try {
+			item=service.borrowItem(itemId, itemName, patronId);
+		}
+		catch(IllegalArgumentException e) {
+			error=e.getMessage();
+		}
+		assertNull(item);
+		assertEquals("The id of the item cannot be negative!",error);
+	}
+	
+	
 	
 }
 
