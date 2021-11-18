6be5e094d199b30a0142ebc25684135018a8d726
sami <77645479+samiad2788@users.noreply.github.com>
2021-11-17 22:10:18 -0400

    started home page

---
+++ LibrarySystem-Frontend/src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="welcomepage">
+    <h1>Welcome to the Library Management System</h1>
+    
+  </div>
+</template>
+<script>
+</script>
+<style>
+#welcomepage {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+</style>
