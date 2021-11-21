94a1e0fbbacb8d8ea22cecaf9aa5d41010166c5a
Julie Chen <julie.chen@mail.mcgill.ca>
2021-11-21 12:56:42 -0500

    added items info - still need to do transition

--- LibrarySystem-Frontend/src/components/ManageItemsPage.vue
+++ LibrarySystem-Frontend/src/components/ManageItemsPage.vue
@@ -7,26 +7,27 @@
     <a class="btn">Items Information</a>
     <a class="btn">Manage Items</a>
 
-<body>
+    <body>
 
-  <div class="itemID">
-    <form>
-      <input type="text" name="itemID" placeholder="Enter Item ID" required="">
-      <button>Enter</button>
-    </form>
-  </div>
-<!--
-  <div class="itemInfo">
-    <h1>Item Information</h1>
-    <button>Borrow Item</button>
-    <button>Return Item</button>
-    <button>Archive Item</button>
-    <button>Damage Item</button>
-    <button>Add New Item</button>
-    <button>Delete Item</button>
-  </div>
--->
-</body>
+      <div class="itemID">
+        <form>
+          <input type="text" name="itemID" placeholder="Enter Item ID" required="">
+          <button>Enter</button>
+        </form>
+      </div>
+
+      <div class="itemInfo">
+        <label for="chk" aria-hidden="true">Item Information</label>
+        <p>Item ID, name, author here</p>
+        <button>Borrow Item</button>
+        <button>Return Item</button>
+        <button>Archive Item</button>
+        <button>Damage Item</button>
+        <button>Add New Item</button>
+        <button>Delete Item</button>
+      </div>
+
+    </body>
   </div>
 </template>
 
@@ -36,81 +37,118 @@
 
 
 <style>
-* {
-	-webkit-transition-property: all;
-	-webkit-transition-duration: .2s;
-  -moz-transition-timing-function: cubic-bezier(100,50,21,6);
-	-moz-transition-property: all;
-  -moz-transition-timing-function: cubic-bezier(100,50,21,6);
-}
+  h1{
+    color:white;
+    font-weight:100;
+    width: 900px;
+    margin: 0 auto;
+    background:rgba(0, 0, 0, 0.6);
+    padding: 10px 20px;
 
-body{
-  padding:75px;
-  text-align:center;
-  font-family: "Courier", sans-serif;
-  background-image: url("../assets/library.jpg");
-}
+  }
 
-h1{
-  color:white;
-  font-weight:100;
-  width: 900px;
-  margin: 0 auto;
-  background:rgba(0, 0, 0, 0.6);
-  padding: 10px 20px;
+  .btn{
+    margin: 20px 0px;
+    color:white;
+    background:rgba(0, 0, 0, 0.5);
+    padding:10px 20px;
+    font-size:12px;
+    text-decoration:none;
+    letter-spacing:2px;
+    text-transform:uppercase;
+  }
 
-}
+  .btn:hover{
+    background:#fff;
+  }
 
-.btn{
-  margin: 20px 0px;
-  color:white;
-  background:rgba(0, 0, 0, 0.5);
-  padding:10px 20px;
-  font-size:12px;
-  text-decoration:none;
-  letter-spacing:2px;
-  text-transform:uppercase;
-}
+  body{
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    gap: 20px;
+    align-items: center;
+    min-height: 100vh;
+    font-family: 'Jost', sans-serif;
+    background-image: url("../assets/library.jpg");
+  }
 
-.btn:hover{
-  background:#fff;
-}
+  .itemID{
+    width: 30%;
+    height: 100%;
+    padding: 10px;
+    background: white;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+  }
 
-.footer{
-  font-size:8px;
-  color:#fff;
-  clear:both;
-  display:block;
-  letter-spacing:5px;
-  border:1px solid #fff;
-  padding:5px;
-  text-decoration:none;
-  width:210px;
-  margin:auto;
-  margin-top:400px;
-}
+  input{
+    width: 60%;
+    height: 20px;
+    background: white;
+    justify-content: center;
+    display: inline;
+    margin: 20px auto;
+    padding: 10px;
+    border: solid;
+    border-color: rgb(183, 201, 241);
+    outline: none;
+    border-radius: 5px;
+  }
+  input:hover{
+    border-color: rgb(135, 169, 241);
+  }
 
-.itemID{
-  width: 500px;
-	height: 800px;
-	background: rgb(255, 255, 255);
-	overflow: hidden;
-	border-radius: 10px;
-	box-shadow: 5px 20px 50px #000;
-}
-/*
-.itemInfo{
-  width: 500px;
-	height: 500px;
-	background: rgb(190, 190, 190);
-	overflow: hidden;
-	border-radius: 10px;
-	box-shadow: 5px 20px 50px #000;
+  button{
+    width: 30%;
+    height: 40px;
+    justify-content: center;
+    display: inline;
+    color: black;
+    background: white;
+      
+    font-size: 1em;
+    font-weight: bold;
+    margin-top: 20px;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+    transition: .2s ease-in;
+    cursor: pointer;
+  }
+  button:hover{
+    background: black;
+      color:white;
+  }
 
-  transform: translateY(-180px);
-	transition: .8s ease-in-out;
-}
-*/
+  label{
+    color: rgb(0, 0, 0);
+    font-size: 2.3em;
+    justify-content: center;
+    display: flex;
+    margin: auto;
+    font-weight: bold;
+    transition: .5s ease-in-out;
+  }
+
+  .itemInfo{
+    width: 60%;
+    height: 100%;
+    padding: 10px;
+    background: white;
+    outline: none;
+    border: none;
+    border-radius: 5px;
+
+    transition: .8s ease-in-out;
+  }
+
+  p {
+    text-align: center;
+  }
 
 </style>
 
