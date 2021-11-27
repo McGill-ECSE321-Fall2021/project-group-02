90267ddd912dbeaf91059a63209b45749a225d42
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2021-11-27 13:39:39 -0500

    Update ViewItemsPage.js

--- LibrarySystem-Frontend/src/store/ViewItemsPage.js
+++ LibrarySystem-Frontend/src/store/ViewItemsPage.js
@@ -7,7 +7,6 @@ var AXIOS = axios.create({
     baseURL: backendUrl,
     headers: { 'Access-Control-Allow-Origin': frontendUrl }
 })
-
 function ItemDto (id, isArchived, isBorrowed, isDamaged){
   this.id = id;
   this.isArchived = isArchived;
@@ -53,24 +52,12 @@ export default {
         response: []
       }
     },
-    
     created: function () {
-      //TEST ITEMS ADDED TO LISTS; DOES NOT TEST BACKEND INTEGRATION
-      const b1 = new BookDto(1, "TEST BOOK", "TEST_AUTHOR")
-      this.books = [b1]
-      const a1 = new AlbumDto(2, "TEST ALBUM", "TEST_ARTIST")
-      this.albums = [a1]
-      const m1 = new MovieDto(3, "TEST MOVIE", "TEST_DIRECTOR")
-      this.movies = [m1]
-      const n1 = new NewspaperDto(4, "TEST NEWSPAPER", new Date)
-      this.newspapers = [n1]
-      const j1 = new JournalDto(5, "TEST JOURNAL", new Date)
-      this.journals = [j1]
-      
       AXIOS.get('/items')
       .then(response => {
         this.items = response.data
       })
+      // Gets all the items from the database
       AXIOS.get('/items/books')
       .then(response => {
         this.books = response.data
@@ -94,8 +81,8 @@ export default {
     },
     methods: 
     {
-      searchItems(searchTerm, searchType){
-        if (searchType.localeCompare("Name") == 0){
+      searchItems(searchTerm, searchType){// Search items by type and term
+        if (searchType.localeCompare("Name") == 0){// Only Include items by the specified name
           AXIOS.get('/items/books/'.concat('?title=', searchTerm)).then(response =>{
             this.books = response.data
           })
@@ -121,7 +108,7 @@ export default {
           AXIOS.get('/items/movies/'.concat('?director=', searchTerm)).then(response =>{
             this.movies = response.data
           })
-        } else if(searchType.localeCompare("None") == 0){
+        } else if(searchType.localeCompare("None") == 0){// Reset displayed items to all the ones in the database
           AXIOS.get('/items')
           .then(response => {
             this.items = response.data
@@ -147,59 +134,7 @@ export default {
             this.journals = response.data
           })
         }
-        
         searchTerm = '';
-        /*
-        if (searchType.localeCompare("ID") == 0) {
-          for(i = 0; i < books.length; i++){
-            if(books[i].id == searchTerm){ 
-              this.books= books[i];
-              albums = [];
-              movies = [];
-              journals = [];
-              newspapers = [];
-            }
-          }
-          for(i = 0; i < albums.length; i++){
-            if(albums[i].id == searchTerm){ 
-              this.albums= albums[i];
-              books = [];
-              movies = [];
-              journals = [];
-              newspapers = [];
-            }
-          }
-          for(i = 0; i < movies.length; i++){
-            if(movies[i].id == searchTerm){ 
-              this.movies= movies[i];
-              albums = [];
-              books = [];
-              journals = [];
-              newspapers = [];
-            }
-          }
-          for(i = 0; i < journals.length; i++){
-            if(journals[i].id == searchTerm){ 
-              this.journals= journals[i];
-              albums = [];
-              movies = [];
-              books = [];
-              newspapers = [];
-            }
-          }
-          for(i = 0; i < newspapers.length; i++){
-            if(newspapers[i].id == searchTerm){ 
-              this.newspapers = newspapers[i];
-              albums = [];
-              movies = [];
-              journals = [];
-              books = [];
-            }
-          }
-          
-        }
-        */
-       
       }
     }
 }
