4996a24ecc8c6c035bc57dd6eeeb2eb8cf262f71
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2021-11-29 20:15:35 -0500

    Add relocation to correct home page

--- LibrarySystem-Frontend/src/components/ViewItemsPage.vue
+++ LibrarySystem-Frontend/src/components/ViewItemsPage.vue
@@ -5,7 +5,18 @@
 				<router-link to="/"><h2>Montreal Library</h2></router-link>
 				<router-link to="/signup"><a class="btn">Sign Up/Log In</a></router-link>
         		<router-link to="/items"><a class="btn">Items Information</a></router-link>
-        		<router-link to="/"><a class="btn">Home</a></router-link>
+				<template v-if="getTypeOfUser().includes('Patron')">
+        			<router-link to="/homeAfterLogin"><a class="btn">Home</a></router-link>
+				</template>
+				<template v-else-if="getTypeOfUser().includes('Librarian')">
+        			<router-link to="/homePageLibrarian"><a class="btn">Home</a></router-link>
+				</template>
+				<template v-else-if="getTypeOfUser().includes('HeadLibrarian')">
+        			<router-link to="/homePageHeadLibrarian"><a class="btn">Home</a></router-link>
+				</template>
+				<template v-else>
+        			<router-link to="/"><a class="btn">Home</a></router-link>
+				</template>
 			</div>
 			<body>
 				<div class=items>
