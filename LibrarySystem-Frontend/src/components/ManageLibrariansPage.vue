ca886428ad574c686f43a67454bf49af6a00b5f4
vy-khahuynh <77684345+vy-Khahuynh@users.noreply.github.com>
2021-11-30 16:50:09 -0500

    Updated buttons

--- LibrarySystem-Frontend/src/components/ManageLibrariansPage.vue
+++ LibrarySystem-Frontend/src/components/ManageLibrariansPage.vue
@@ -96,7 +96,7 @@ body{
 }
 .header{
 	width:100%;
-	height: 100px;
+	height: 130px;
 	position: fixed;
 	top: 0;
 	left: 0;
@@ -194,7 +194,7 @@ th:nth-child(5) {
 }
 .btn{
   float:right;
-  margin: 45px 5px;
+  margin: 65px 5px;
   color:white;
   background:rgba(0, 0, 0, 0.8);
   padding:10px 20px;
