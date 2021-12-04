f0d1c7ab188abe231516570d8f7706a63a8092de
johnpark3002 <59747857+johnpark3002@users.noreply.github.com>
2021-12-04 13:55:54 -0500

    Update CreateOnlineAccountController.java

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/CreateOnlineAccountController.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/CreateOnlineAccountController.java
@@ -23,6 +23,21 @@ import ca.mcgill.ecse321.librarysystem.service.CreateOnlineAccountService;
 public class CreateOnlineAccountController {
 	@Autowired
 	CreateOnlineAccountService service;
+	
+	/**
+	* Creates an online account for a new user
+	* @param firstName The first name of the new user
+	* @param lastName The last name of the new user
+	* @param address The address of the new user
+	* @param city The city of the new user
+	* @param username The username of the new user
+	* @param password The password of the new user
+	* @param email The email of the new user
+	*
+	* @return OnlineAccountDto
+	*
+	* @author Sami
+	*/
 
 	@PostMapping(value = { "/onlineAccountNew/{firstName}/{lastName}/{address}/{city}/{username}/{password}/{email}",
 			"/onlineAccountNew/{firstName}/{lastName}/{address}/{city}/{username}/{password}/{email}/" })
@@ -35,6 +50,16 @@ public class CreateOnlineAccountController {
 				email, false);
 		return convertToDto(account);
 	}
+	
+	/**
+	* Logs in the user with the username and password
+	* @param username The username that the patron uses to log in
+	* @param password The password that the patron uses to log in
+	*
+	* @return OnlineAccountDto
+	* 
+	* @author Sami
+	*/
 
 	@PostMapping(value = { "/logIn/{username}/{password}", "/logIn/{username}/{password}/" })
 	public OnlineAccountDto logIn(@PathVariable(name = "username") String username,
@@ -42,6 +67,12 @@ public class CreateOnlineAccountController {
 		OnlineAccount account = service.logIn(username, password, true);
 		return convertToDto(account);
 	}
+	
+	/**
+	* Signs out the currently logged in user
+	*
+	* @author Sami
+	*/
 
 	@PostMapping(value = { "/signOut", "/signOut/" })
 	public void signOut() throws IllegalArgumentException {
@@ -83,6 +114,18 @@ public class CreateOnlineAccountController {
 	public int getLoggedInAccountID() {
 		return service.getloggedInAccountID();
 	}
+	
+	/**
+	* Creates an online account for an existing user
+	* @param id The ID that was generated for the existing user
+	* @param username The username that the existing user created
+	* @param password The password that the existing user created
+	* @param email The email of the existing user
+	*
+	* @return OnlineAccountDto
+	*
+	* @author Vy-Kha
+	*/
 
 	@PostMapping(value = { "/onlineAccountExisting/{id}/{username}/{password}/{email}",
 			"/onlineAccountExisting/{id}/{username}/{password}/{email}/" })
@@ -93,24 +136,59 @@ public class CreateOnlineAccountController {
 		OnlineAccount account = service.createOnlineAccountExistingUser(id, username, password, email, false);
 		return convertToDto(account);
 	}
+	
+	/**
+	* Deletes the online account given a username
+	* @param username The username associated with the online account
+	* @param password The password associated with the online account
+	*
+	* @author Hyunbum Cho
+	*/
 
 	@DeleteMapping(value = { "/deleteOnlineAccountUsername", "/deleteOnlineAccountUsername/" })
 	public void deleteOnlineAccountUsername(@RequestParam(name = "username") String username,
 			@RequestParam(name = "password") String password) throws IllegalArgumentException {
 		service.deleteOnlineAccountUsername(username, password);
 	}
+	
+	/**
+	* Deletes the online account given an email
+	* @param email The email associated with the online account
+	* @param password The password associated with the online account
+	*
+	* @author Hyunbum Cho
+	*/
 
 	@DeleteMapping(value = { "/deleteOnlineAccountEmail", "/deleteOnlineAccountEmail/" })
 	public void deleteOnlineAccountEmail(@RequestParam(name = "email") String email,
 			@RequestParam(name = "password") String password) throws IllegalArgumentException {
 		service.deleteOnlineAccountUsername(email, password);
 	}
+	
+	/**
+	* Deletes the online account given an ID
+	* @param id The ID associated with the online account
+	* @param password The password associated with the online account
+	*
+	* @author Hyunbum Cho
+	*/
 
 	@DeleteMapping(value = { "/deleteOnlineAccount", "/deleteOnlineAccount/" })
 	public void deleteOnlineAccount(@RequestParam(name = "id") int id,
 			@RequestParam(name = "password") String password) throws IllegalArgumentException {
 		service.deleteOnlineAccount(id, password);
 	}
+	
+	/**
+	* Changes the password for a given online account
+	* @param id The ID associated with the online account
+	* @param password The old password associated with the online account
+	* @param newPassword The new password to be associated with the online account
+	*
+	* @return OnlineAccountDto
+	*
+	* @author Hyunbum Cho
+	*/
 
 	@PutMapping(value = { "/changePassword", "/changePassword/" })
 	public OnlineAccountDto changePassword(@RequestParam(name = "id") int id,
@@ -119,6 +197,17 @@ public class CreateOnlineAccountController {
 		OnlineAccount account = service.changePassword(id, password, newPassword);
 		return convertToDto(account);
 	}
+	
+	/**
+	* Changes the email for a given online account
+	* @param id The ID associated with the online account
+	* @param password The password associated with the online account
+	* @param newEmail The new email to be associated with the online account
+	*
+	* @return OnlineAccountDto
+	*
+	* @author Hyunbum Cho
+	*/
 
 	@PutMapping(value = { "/changeEmail", "/changeEmail/" })
 	public OnlineAccountDto changeEmail(@RequestParam(name = "id") int id,
@@ -127,6 +216,17 @@ public class CreateOnlineAccountController {
 		OnlineAccount account = service.changeEmail(id, password, newEmail);
 		return convertToDto(account);
 	}
+	
+	/**
+	* Changes the username for a given online account
+	* @param id The ID associated with the online account
+	* @param password The password associated with the online account
+	* @param newUsername The new username to be associated with the online account
+	*
+	* @return OnlineAccountDto
+	*
+	* @author Hyunbum Cho
+	*/
 
 	@PutMapping(value = { "/changeUsername", "/changeUsername/" })
 	public OnlineAccountDto changeUsername(@RequestParam(name = "id") int id,
