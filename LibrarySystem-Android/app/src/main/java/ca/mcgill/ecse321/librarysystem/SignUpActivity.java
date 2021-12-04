e1953dc4b416e1a02e792a9113e7c6b49dcee7b1
sami <77645479+samiad2788@users.noreply.github.com>
2021-12-04 12:07:58 -0400

    added login and sign up layouts

--- LibrarySystem-Android/app/src/main/java/ca/mcgill/ecse321/librarysystem/SignUpActivity.java
+++ LibrarySystem-Android/app/src/main/java/ca/mcgill/ecse321/librarysystem/SignUpActivity.java
@@ -1,7 +1,9 @@
 package ca.mcgill.ecse321.librarysystem;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends Activity {
 
@@ -9,5 +11,16 @@ public class SignUpActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_page);
+    }
+
+    /**
+     * Redirects to the intro page
+     *
+     * @param view the view that calls the method
+     * @author Sami Ait Ouahmane
+     */
+    public void introRedirect(View view){
+        Intent i = new Intent(this, IntroActivity.class);
+        startActivity(i);
     }
 }
