bbf68a900db03ce5bbab4f14314c14582be72526
johnpark3002 <59747857+johnpark3002@users.noreply.github.com>
2021-12-04 14:28:40 -0500

    Update EmploymentRestController.java

--- LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/EmploymentRestController.java
+++ LibrarySystem-Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/EmploymentRestController.java
@@ -33,9 +33,12 @@ public class EmploymentRestController {
 	ManagingEmploymentService service;
 	
 	/**
-	 * @author vy-khahuynh
-	 * @param id id of the user
+	 * Gets all librarians sharing the same first and last name
+	 * @param firstname The first name of the librarian
+	 * @param lastname The last name of the librarian
+	 * 
 	 * @return list of all librarians sharing the same first and last name
+	 * 
 	 * @author vy-khahuynh
 	 */
 	@GetMapping(value = { "/librariansfnln", "/librariansfnln/" })
@@ -44,9 +47,11 @@ public class EmploymentRestController {
 	}
 	
 	/**
-	 * @author vy-khahuynh
-	 * @param id id of the user
-	 * @return list of all librarians sharing the same first and last name
+	 * Gets all librarians in a sorted manner depending on the mode
+	 * @param mode The mode used to sort the list of librarians
+	 * 
+	 * @return sorted list of all librarians depending on the mode
+	 * 
 	 * @author vy-khahuynh
 	 */
 	@GetMapping(value = { "/librariansSort", "/librariansSort/" })
@@ -55,9 +60,11 @@ public class EmploymentRestController {
 	}
 	
 	/**
-	 * @author vy-khahuynh
-	 * @param id id of the user
-	 * @return list of all librarians sharing the same first and last name
+	 * Gets all librarians whose names match the search query 
+	 * @param librarianName The name of the librarians that are we are searching for
+	 * 
+	 * @return list of all librarians matching the search query
+	 * 
 	 * @author vy-khahuynh
 	 */
 	@GetMapping(value = { "/librarianSearch", "/librarianSearch/" })
@@ -67,9 +74,10 @@ public class EmploymentRestController {
 	
 
 	/**
-	 * @author vy-khahuynh
-	 * @param id id of the user
-	 * @return list of all librarians sharing the same first and last name
+	 * Gets all librarians in the system
+	 * 
+	 * @return list of all librarians 
+	 * 
 	 * @author vy-khahuynh
 	 */
 	@GetMapping(value = { "/librarians", "/librarians/" })
@@ -78,11 +86,12 @@ public class EmploymentRestController {
 	}
 	
 	/**
-	 * @author vy-khahuynh
-	 * @param id id of the user
-	 * @param fn first name to be searched
-	 * @return list of all librarians sharing the same first name
+	 * Gets all librarians whose first names match the search query
+	 * @param firstName The first name of the librarians to be searched
 	 * 
+	 * @return list of all librarians sharing the same first name
+	 *
+	 * @author Vy-Kha Huynh
 	 */
 	@GetMapping(value = { "/librariansfn", "/librariansfn/" })
 	public List<LibrarianDto> getAllLibrariansByFirstName(@RequestParam (name="firstname") String fn) throws IllegalArgumentException{
@@ -90,10 +99,12 @@ public class EmploymentRestController {
 	}
 	
 	/**
-	 * @author vy-khahuynh
-	 * @param id id of the user
-	 * @param ln last name to be searched
+	 * Gets all librarians whose last names match the search query
+	 * @param lastname The last name of the librarians to be searched
+	 *
 	 * @return list of all librarians sharing the same last name
+	 *
+	 * @author Vy-Kha Huynh
 	 */
 	@GetMapping(value = { "/librariansln", "/librariansln/" })
 	public List<LibrarianDto> getAllLibrariansByLastName(@RequestParam (name="lastname") String ln) throws IllegalArgumentException{
@@ -101,10 +112,12 @@ public class EmploymentRestController {
 	}
 	
 	/**
-	 * @author vy-khahuynh
-	 * @param hlid id of headlibrarian
-	 * @param libid id of librarian to be searched
-	 * @return librarian dto object
+	 * Gets the particular librarian associated with the ID
+	 * @param libid The ID of the librarian to be searched
+	 *
+	 * @return Librarian that is being searched for
+	 * 
+	 * @author Vy-Kha Huynh
 	 */
 	@GetMapping(value= {"/librariansid" , "/librariansid/"})
 	public LibrarianDto getLibrarianByID(@RequestParam(name="LibID") int libid) throws IllegalArgumentException{
@@ -112,10 +125,28 @@ public class EmploymentRestController {
 		return convertToDto(l);
 	}
 	
+	/**
+	 * Gets the weekly schedule of the librarian associated with the ID
+	 * @param libid The ID of the librarian to be searched
+	 *
+	 * @return List of librarian's weekly schedules
+	 * 
+	 * @author John Park
+	 */
+	
 	@GetMapping(value= {"/schedulesweekly", "/schedulesweekly/" })
 	public List<WeeklyScheduleDto> getAllWeeklySchedulesByID(@RequestParam(name="LibID") int libid) throws IllegalArgumentException {
 		return service.getWeeklySchedulesByID(libid).stream().map(l -> convertToDto(l)).collect(Collectors.toList());
 	}
+	
+	/**
+	 * Gets the daily schedules of the librarian associated with the ID
+	 * @param libid The ID of the librarian to be searched
+	 *
+	 * @return List of librarian's daily schedules
+	 * 
+	 * @author John Park
+	 */
 	
 	@GetMapping(value= {"/schedulesdaily" , "/schedulesdaily/"})
 	public List<DailyScheduleDto> getDailySchedulesByID(@RequestParam(name="LibID")int libid) throws IllegalArgumentException{
@@ -123,17 +154,21 @@ public class EmploymentRestController {
 	}
 	
 	/**
-	 * @author vy-khahuynh
-	 * @param id id of the user
-	 * @param firstname first name of the librarian 
-	 * @param lastname last name of the librarian
-	 * @param address address of librarian
-	 * @param city city of residence of the librarian
-	 * @param email email to be associated with the online account
-	 * @param username username to be associated with the online account
-	 * @param password password o be associated with the online account
+	 * Creates a librarian
+	 * @param userID The ID of the user
+	 * @param firstname The first name of the librarian 
+	 * @param lastname The last name of the librarian
+	 * @param address The address of librarian
+	 * @param city The city of residence of the librarian
+	 * @param email The email to be associated with the online account
+	 * @param username The username to be associated with the online account
+	 * @param password The password o be associated with the online account
+	 *
 	 * @return new librarian with the inputs as attributes
+	 *
 	 * @throws IllegalArgumentException
+	 *
+	 * @author Vy-Kha Huynh
 	 */
 	@PostMapping(value = { "/createLibrarian/{userID}/{firstname}/{lastname}/{address}/{city}", "/createLibrarian/{userID}/{firstname}/{lastname}/{address}/{city}/" })
 	public LibrarianDto createLibrarian(@PathVariable(name="userID")int userID,@PathVariable(name="firstname") String firstname,
@@ -143,12 +178,41 @@ public class EmploymentRestController {
 		return convertToDto(l);
 	}
 	
+	/**
+	 * Creates a daily schedule for the librarian
+	 * @param userID The ID of the user
+	 * @param librarianID The ID of the librarian 
+	 * @param weekday The corresponding day of the daily schedule
+	 * @param startTime The start time of the daily schedule
+	 * @param endTime The end time of the daily schedule
+	 *
+	 * @return New daily schedule with the inputs as attributes
+	 *
+	 * @throws IllegalArgumentException
+	 *
+	 * @author John Park
+	 */
+	
 	@PostMapping(value = { "/createDailySchedule/{userID}/{librarianID}/{weekday}/{startTime}/{endTime}", "/createDailySchedule/{userID}/{librarianID}/{weekday}/{startTime}/{endTime}/" })
 	public DailyScheduleDto createDailySchedule(@PathVariable(name="userID") int userID, @PathVariable(name="librarianID")int librarianID, @PathVariable(name="weekday") WeekDay day, 
 			@PathVariable(name="startTime") Time startTime, @PathVariable(name="endTime") Time endTime) throws IllegalArgumentException {
 				DailySchedule ds = service.createDailySchedule(userID, librarianID, day, startTime, endTime);
 				return convertToDto(ds);
 			}
+	
+	/**
+	 * Creates a weekly schedule for the librarian
+	 * @param userID The ID of the user
+	 * @param startDate The start date of the weekly schedule
+	 * @param endDate The end date of the weekly scheudle
+	 * @param librarianID The ID of the librarian 
+	 *
+	 * @return New weekly schedule with the inputs as attributes
+	 *
+	 * @throws IllegalArgumentException
+	 *
+	 * @author John Park
+	 */
 	
 	@PostMapping(value = { "/createWeeklySchedule/{userID}/{startDate}/{endDate}/{librarianID}", "/createWeeklySchedule/{userID}/{startDate}/{endDate}/{librarianID}/" })
 	public WeeklyScheduleDto createWeeklySchedule(@PathVariable(name="userID") int userID, @PathVariable(name="startDate") Date startDate, 
@@ -158,18 +222,15 @@ public class EmploymentRestController {
 	}
 	
 	/**
-	 * @author vy-khahuynh
-	 * @param id
-	 * @param firstname
-	 * @param lastname
-	 * @param address
-	 * @param city
-	 * @param email
-	 * @param username
-	 * @param password
-	 * @return
+	 * Creates the head librarian with predefined attributes for the system
+	 * 
+	 * @return The head librarian of the library
+	 *
 	 * @throws IllegalArgumentException
+	 *
+	 * @author Vy-Kha Huynh
 	 */
+	
 	@PostMapping(value = { "/createHeadLibrarian", "/createHeadLibrarian/" })
 	public HeadLibrarianDto createHeadLibrarian() throws IllegalArgumentException {
 		HeadLibrarian hl = service.createHeadLibrarian();
@@ -179,20 +240,29 @@ public class EmploymentRestController {
 	
 	
 	/**
-	 * @author vy-khahuynh
-	 * @param hlid head librarian id attempting to delete the librarian
-	 * @param libid id of librarian to be deleted
+	 * Deletes a librarian
+	 * @param hlid The ID of the head librarian attempting to delete the librarian
+	 * @param libid The ID of librarian to be deleted
+	 * 
+	 * @throws IllegalArgumentException
+	 * 
+	 * @author Vy-Kha Huynh
 	 */
+	
 	@DeleteMapping(value = {"/deleteLibrarian/{userID}","/deleteLibrarian/{userID}/"})
 	public void deleteLibrarian(@PathVariable(name="userID") int hlid,@RequestParam(name="LibID") int libid) throws IllegalArgumentException {
 		service.deleteLibrarian(hlid, libid);
 	}
 	
 	/**
-	 * @author vy-khahuynh
-	 * @param l librarian object to be converted to librarianDto
+	 * Converts Librarian object to LibrarianDto object
+	 * @param l Librarian object to be converted to librarianDto
+	 * 
 	 * @return librarianDto object of librarian l
+	 *
+	 * @author Vy-Kha Huynh
 	 */
+	
 	private LibrarianDto convertToDto(Librarian l) {
 		if (l == null) {
 			throw new IllegalArgumentException("There is no such Librarian!");
@@ -202,10 +272,14 @@ public class EmploymentRestController {
 	}
 	
 	/**
-	 * @author vy-khahuynh
-	 * @param l librarian object to be converted to librarianDto
-	 * @return librarianDto object of librarian l
+	 * Converts HeadLibrarian object to HeadLibrarianDto object
+	 * @param hl HeadLibrarian object to be converted to HeadLibrarianDto
+	 * 
+	 * @return HeadLibrarianDto object of HeadLibrarian hl
+	 *
+	 * @author Vy-Kha Huynh
 	 */
+	
 	private HeadLibrarianDto convertToDto(HeadLibrarian hl) {
 		if (hl == null) {
 			throw new IllegalArgumentException("There is no such Librarian!");
@@ -214,6 +288,15 @@ public class EmploymentRestController {
 		return hlDto;	
 	}
 	
+	/**
+	 * Converts DailySchedule object to DailyScheduleDto object
+	 * @param ds DailySchedule object to be converted to DailyScheduleDto object
+	 * 
+	 * @return DailyScheduleDto object of DailySchedule ds
+	 *
+	 * @author John Park
+	 */
+	
 	private DailyScheduleDto convertToDto(DailySchedule ds) {
 		if (ds == null) {
 			throw new IllegalArgumentException("Daily Schedule does not exist.");
@@ -221,6 +304,15 @@ public class EmploymentRestController {
 		DailyScheduleDto dsDto = new DailyScheduleDto(ds.getDay(), ds.getStartTime(), ds.getEndTime(), ds.getId());
 		return dsDto;	
 	}
+	
+	/**
+	 * Converts WeeklySchedule object to WeeklyScheduleDto object
+	 * @param ws WeeklySchedule object to be converted to WeeklyScheduleDto object
+	 * 
+	 * @return WeeklyScheduleDto object of WeeklySchedule ds
+	 *
+	 * @author John Park
+	 */
 	
 	private WeeklyScheduleDto convertToDto(WeeklySchedule ws) {
 		if (ws == null) {
