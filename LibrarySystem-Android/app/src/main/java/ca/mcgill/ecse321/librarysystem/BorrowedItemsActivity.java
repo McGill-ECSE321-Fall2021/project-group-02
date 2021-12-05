3272a9a69aca41f3f2700b323b8fec95e1cf0420
vy-khahuynh <77684345+vy-Khahuynh@users.noreply.github.com>
2021-12-05 14:32:48 -0500

    viewBorrowedItems works, needs formatting

--- LibrarySystem-Android/app/src/main/java/ca/mcgill/ecse321/librarysystem/BorrowedItemsActivity.java
+++ LibrarySystem-Android/app/src/main/java/ca/mcgill/ecse321/librarysystem/BorrowedItemsActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -15,14 +13,13 @@ import android.widget.TextView;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 
-import java.util.ArrayList;
-import java.util.List;
+import cz.msebera.android.httpclient.Header;
 
-import cz.msebera.android.httpclient.entity.mime.Header;
+
 
 public class BorrowedItemsActivity extends Activity {
     String error = "";
@@ -45,174 +42,164 @@ public class BorrowedItemsActivity extends Activity {
         setStyle();
     }
 
-    private void refreshErrorMessage() {
-        // set the error message
-        TextView tvError = (TextView) findViewById(R.id.error);
-        tvError.setText(error);
-
-        if (error == null || error.length() == 0) {
-            tvError.setVisibility(View.GONE);
-        } else {
-            tvError.setVisibility(View.VISIBLE);
-        }
-    }
+//    private void refreshErrorMessage() {
+//        // set the error message
+//        TextView tvError = (TextView) findViewById(R.id.error);
+//        tvError.setText(error);
+//
+//        if (error == null || error.length() == 0) {
+//            tvError.setVisibility(View.GONE);
+//        } else {
+//            tvError.setVisibility(View.VISIBLE);
+//        }
+//    }
 
     public void populate() throws JSONException {
         TableLayout stk = (TableLayout) findViewById(R.id.content);
+        HttpUtils.get("onlineAccountLoggedIn/", new RequestParams(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response){
+                try {
+                    id = response.getInt("userId");
+                    RequestParams rp = new RequestParams();
+                    rp.add("id",Integer.toString(id));
+                    HttpUtils.get("borrowedItems/books", rp , new JsonHttpResponseHandler() {
+                        public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                            try {
+                                books = response;
+                                for (int i = 0; i < books.length(); i++) {
+                                    TableRow tbrow = new TableRow(BorrowedItemsActivity.this);
+                                    TextView t1v = new TextView(BorrowedItemsActivity.this);
+                                    t1v.setText(books.getJSONObject(i).getString("title"));
+                                    t1v.setTextColor(Color.BLACK);
+                                    t1v.setGravity(Gravity.CENTER);
+                                    tbrow.addView(t1v);
+                                    TextView t2v = new TextView(BorrowedItemsActivity.this);
+                                    t2v.setText(books.getJSONObject(i).getString("author"));
+                                    t2v.setTextColor(Color.BLACK);
+                                    t2v.setGravity(Gravity.CENTER);
+                                    tbrow.addView(t2v);
+                                    TextView t3v = new TextView(BorrowedItemsActivity.this);
+                                    t3v.setText(books.getJSONObject(i).getString("id"));
+                                    t3v.setTextColor(Color.BLACK);
+                                    t3v.setGravity(Gravity.CENTER);
+                                    tbrow.addView(t3v);
+                                    stk.addView(tbrow, curr);
+                                    curr++;
+                                }
+                                curr++;
+                                curr++;
 
-        HttpUtils.get("onlineAccountLoggedInID", new RequestParams(), new JsonHttpResponseHandler() {
-            public void onSuccess(int statusCode, Header[] headers, int response) {
-                id = response;
-                refreshErrorMessage();
+                            }catch(JSONException e){
+                                error += e.getMessage();
+                            }
+                        }
+
+                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                            try {
+                                error += errorResponse.get("message").toString();
+                            } catch (JSONException e) {
+                                error += e.getMessage();
+                            }
+                        }
+                    });
+
+                    HttpUtils.get("borrowedItems/albums", rp , new JsonHttpResponseHandler() {
+                        public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                            try {
+                                albums = response;
+                                for (int i = 0; i < albums.length(); i++) {
+                                    TableRow tbrow = new TableRow(BorrowedItemsActivity.this);
+                                    TextView t1v = new TextView(BorrowedItemsActivity.this);
+                                    t1v.setText(albums.getJSONObject(i).getString("title"));
+                                    t1v.setTextColor(Color.BLACK);
+                                    t1v.setGravity(Gravity.CENTER);
+                                    tbrow.addView(t1v);
+                                    TextView t2v = new TextView(BorrowedItemsActivity.this);
+                                    t2v.setText(albums.getJSONObject(i).getString("artist"));
+                                    t2v.setTextColor(Color.BLACK);
+                                    t2v.setGravity(Gravity.CENTER);
+                                    tbrow.addView(t2v);
+                                    TextView t3v = new TextView(BorrowedItemsActivity.this);
+                                    t3v.setText(albums.getJSONObject(i).getString("id"));
+                                    t3v.setTextColor(Color.BLACK);
+                                    t3v.setGravity(Gravity.CENTER);
+                                    tbrow.addView(t3v);
+                                    stk.addView(tbrow, curr);
+                                    curr++;
+                                }
+
+                                curr++;
+                                curr++;
+
+                            }catch(JSONException e){
+                                error += e.getMessage();
+                            }
+                        }
+
+                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                            try {
+                                error += errorResponse.get("message").toString();
+                            } catch (JSONException e) {
+                                error += e.getMessage();
+                            }
+                        }
+                    });
+
+                    HttpUtils.get("borrowedItems/movies", rp , new JsonHttpResponseHandler() {
+                        public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                            try {
+                                movies = response;
+                                for (int i = 0; i < movies.length(); i++) {
+                                    TableRow tbrow = new TableRow(BorrowedItemsActivity.this);
+                                    TextView t1v = new TextView(BorrowedItemsActivity.this);
+                                    t1v.setText(movies.getJSONObject(i).getString("title"));
+                                    t1v.setTextColor(Color.BLACK);
+                                    t1v.setGravity(Gravity.CENTER);
+                                    tbrow.addView(t1v);
+                                    TextView t2v = new TextView(BorrowedItemsActivity.this);
+                                    t2v.setText(movies.getJSONObject(i).getString("director"));
+                                    t2v.setTextColor(Color.BLACK);
+                                    t2v.setGravity(Gravity.CENTER);
+                                    tbrow.addView(t2v);
+                                    TextView t3v = new TextView(BorrowedItemsActivity.this);
+                                    t3v.setText(movies.getJSONObject(i).getString("id"));
+                                    t3v.setTextColor(Color.BLACK);
+                                    t3v.setGravity(Gravity.CENTER);
+                                    tbrow.addView(t3v);
+                                    stk.addView(tbrow,curr);
+                                    curr++;
+                                }
+
+                            }catch(JSONException e){
+                                error += e.getMessage();
+                            }
+                        }
+
+                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                            try {
+                                error += errorResponse.get("message").toString();
+                            } catch (JSONException e) {
+                                error += e.getMessage();
+                            }
+                        }
+                    });
+
+
+                } catch (JSONException e){
+                    error = e.getMessage();
+                }
             }
 
+            @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 try{
                     error += errorResponse.get("message").toString();
                 } catch (JSONException e){
                     error += e.getMessage();
                 }
-                refreshErrorMessage();
             }
         });
-
-//        HttpUtils.get("borrowedItems/books", new RequestParams(id), new JsonHttpResponseHandler() {
-//            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-//            System.out.println(statusCode);
-//                //                books = response;
-//                refreshErrorMessage();
-//            }
-//
-//            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-//                try {
-//                    error += errorResponse.get("message").toString();
-//                } catch (JSONException e) {
-//                    error += e.getMessage();
-//                }
-//                refreshErrorMessage();
-//            }
-//        });
-        for (int i = 0; i < 4; i++) {
-            TableRow tbrow = new TableRow(this);
-            TextView t1v = new TextView(this);
-            t1v.setText("Ok");
-            t1v.setTextColor(Color.BLACK);
-            t1v.setGravity(Gravity.CENTER);
-            tbrow.addView(t1v);
-            TextView t2v = new TextView(this);
-            t2v.setText("Hi");
-            t2v.setTextColor(Color.BLACK);
-            t2v.setGravity(Gravity.CENTER);
-            tbrow.addView(t2v);
-            TextView t3v = new TextView(this);
-            t3v.setText("Hello");
-            t3v.setTextColor(Color.BLACK);
-            t3v.setGravity(Gravity.CENTER);
-            tbrow.addView(t3v);
-            stk.addView(tbrow, curr);
-            curr++;
-        }
-
-        curr++;
-
-//        for (int i = 0; i < books.length(); i++) {
-//            TableRow tbrow = new TableRow(this);
-//            TextView t1v = new TextView(this);
-//            t1v.setText(books.getJSONObject(i).getString("title"));
-//            t1v.setTextColor(Color.BLACK);
-//            t1v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t1v);
-//            TextView t2v = new TextView(this);
-//            t2v.setText(books.getJSONObject(i).getString("author"));
-//            t2v.setTextColor(Color.BLACK);
-//            t2v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t2v);
-//            TextView t3v = new TextView(this);
-//            t3v.setText(books.getJSONObject(i).getInt("itemID"));
-//            t3v.setTextColor(Color.BLACK);
-//            t3v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t3v);
-//            stk.addView(tbrow, curr);
-//            curr++;
-//        }
-//
-//        curr++;
-
-//        HttpUtils.get("borrowedItems/albums", new RequestParams(id), new JsonHttpResponseHandler() {
-//            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-//                albums = response;
-//                refreshErrorMessage();
-//            }
-//
-//            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-//                try {
-//                    error += errorResponse.get("message").toString();
-//                } catch (JSONException e) {
-//                    error += e.getMessage();
-//                }
-//                refreshErrorMessage();
-//            }
-//        });
-//
-//        for (int i = 0; i < albums.length(); i++) {
-//            TableRow tbrow = new TableRow(this);
-//            TextView t1v = new TextView(this);
-//            t1v.setText(albums.getJSONObject(i).getString("title"));
-//            t1v.setTextColor(Color.BLACK);
-//            t1v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t1v);
-//            TextView t2v = new TextView(this);
-//            t2v.setText(books.getJSONObject(i).getString("artist"));
-//            t2v.setTextColor(Color.BLACK);
-//            t2v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t2v);
-//            TextView t3v = new TextView(this);
-//            t3v.setText(books.getJSONObject(i).getInt("itemID"));
-//            t3v.setTextColor(Color.BLACK);
-//            t3v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t3v);
-//            stk.addView(tbrow, curr);
-//            curr++;
-//        }
-
-//        curr++;
-//
-//        HttpUtils.get("borrowedItems/movies", new RequestParams(id), new JsonHttpResponseHandler() {
-//            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-//                movies = response;
-//                refreshErrorMessage();
-//            }
-//
-//            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-//                try {
-//                    error += errorResponse.get("message").toString();
-//                } catch (JSONException e) {
-//                    error += e.getMessage();
-//                }
-//                refreshErrorMessage();
-//            }
-//        });
-//
-//        for (int i = 0; i < movies.length(); i++) {
-//            TableRow tbrow = new TableRow(this);
-//            TextView t1v = new TextView(this);
-//            t1v.setText(movies.getJSONObject(i).getString("title"));
-//            t1v.setTextColor(Color.BLACK);
-//            t1v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t1v);
-//            TextView t2v = new TextView(this);
-//            t2v.setText(movies.getJSONObject(i).getString("director"));
-//            t2v.setTextColor(Color.BLACK);
-//            t2v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t2v);
-//            TextView t3v = new TextView(this);
-//            t3v.setText(movies.getJSONObject(i).getInt("itemID"));
-//            t3v.setTextColor(Color.BLACK);
-//            t3v.setGravity(Gravity.CENTER);
-//            tbrow.addView(t3v);
-//            stk.addView(tbrow, curr);
-//            curr++;
-//        }
     }
 
     /**
